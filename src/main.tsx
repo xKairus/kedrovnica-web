f4719fcac27b44dd7c86324766ea42951c2abf02
Anton Boldyrev <boldyrev.webdev@gmail.com>
2025-12-18 20:17:10 +0500

    fix: add BrowserRouter to main.tsx to enable routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import App from "@/App.tsx"
-
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom" // <-- 1. Импортируй это
+import App from "./App.tsx"
 import "./app/styles/variables.css"
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
