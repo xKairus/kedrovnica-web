9b80055184f9dc716197eac1b159b4320d251feb
Anton Boldyrev <boldyrev.webdev@gmail.com>
2025-12-18 14:58:32 +0500

    fix: remove default vite styles

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,24 +1,21 @@
-import { ButtonHTMLAttributes, ReactNode } from 'react';
-import clsx from 'clsx'; 
-import s from './Button.module.css';
+import { ButtonHTMLAttributes, ReactNode } from "react"
+import clsx from "clsx"
+import s from "./Button.module.css"
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'primary' | 'outline';
-  children: ReactNode;
+  variant?: "primary" | "outline"
+  children: ReactNode
 }
 
-export const Button = ({ 
-  className, 
-  variant = 'primary', 
-  children, 
-  ...props 
+export const Button = ({
+  className,
+  variant = "primary",
+  children,
+  ...props
 }: ButtonProps) => {
   return (
-    <button 
-      className={clsx(s.button, s[variant], className)} 
-      {...props}
-    >
+    <button className={clsx(s.button, s[variant], className)} {...props}>
       {children}
     </button>
-  );
-};
+  )
+}
